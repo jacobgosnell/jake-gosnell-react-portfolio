52d6841f4c0a075124aae1a44511359bf053b88d
Jake <gosnelljacob@gmail.com>
2019-06-27 09:09:15 -0700

    NavLinks Updated

---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function(props) {
+    // Data that we'll need:
+    // - background image: thumb_image_url
+    // - logo img
+    // - description: description
+    // - id: id
+
+    const { id, description, thumb_image_url, logo } = props.item;
+    return (
+        <div className="portfolio-item-wrapper">
+        <img src={thumb_image_url} />
+        <img src={logo} />
+            <div>{description}</div>
+
+            <Link to={`/portfolio/${id}`}>Link</Link>
+        </div>
+    )
+}
