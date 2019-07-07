cbf0a82debfe91d5735063feac17370d7a294626
Jake <gosnelljacob@gmail.com>
2019-07-06 19:06:26 -0700

    API setup for portfolio-manager page

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            
+            portfolioItems: []
         }
     }
 
+    getPortfolioItems() { // this function has to be called by a lifecycle hook
+        axios.get("https://jake.devcamp.space/portfolio/portfolio_items", { 
+            withCredentials: true 
+        }).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+                // use spread operator b/c we need all the portfolio items
+                // to be split up into 12 different items
+            })
+        }).catch(error => {
+            console.log("error in getPortfolioItems", error);
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
+    componentDidMount
     render() {
         return (
             <div>
