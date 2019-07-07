68f2b07cb1365ee3814ee6d8bffe4949a258e428
Jake <gosnelljacob@gmail.com>
2019-07-06 19:53:57 -0700

    API setup with axios

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            
+            portfolioItems: []
         }
     }
 
+    getPortfolioItems() { 
+        axios.get("https://jake.devcamp.space/portfolio/portfolio_items", { 
+            withCredentials: true 
+        }).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            })
+        }).catch(error => {
+            console.log("error in getPortfolioItems", error);
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
+    componentDidMount
     render() {
         return (
             <div>
