5a438f3950175017429da5b6171b465667ef94c4
Jake <gosnelljacob@gmail.com>
2019-07-07 14:28:27 -0700

    passed portfolioItem data into a new component on the portfolio manager page

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 
 export default class PortfolioManager extends Component {
     constructor(props) {
@@ -10,21 +12,12 @@ export default class PortfolioManager extends Component {
         }
     }
 
-<<<<<<< HEAD
     getPortfolioItems() { 
-=======
-    getPortfolioItems() { // this function has to be called by a lifecycle hook
->>>>>>> cbf0a82debfe91d5735063feac17370d7a294626
         axios.get("https://jake.devcamp.space/portfolio/portfolio_items", { 
             withCredentials: true 
         }).then(response => {
             this.setState({
                 portfolioItems: [...response.data.portfolio_items]
-<<<<<<< HEAD
-=======
-                // use spread operator b/c we need all the portfolio items
-                // to be split up into 12 different items
->>>>>>> cbf0a82debfe91d5735063feac17370d7a294626
             })
         }).catch(error => {
             console.log("error in getPortfolioItems", error);
@@ -45,7 +38,7 @@ export default class PortfolioManager extends Component {
                     </div>
 
                     <div className="right-side">
-                        <h1>Portfolio sidebar....</h1>
+                        <PortfolioSidebarList data={this.state.portfolioItems} />
                     </div>
                 </div>
             </div>
