394c96d8d0bb9ae37fa1adbf3492698715fcfc48
Jake <gosnelljacob@gmail.com>
2019-07-12 18:57:59 -0700

    installed and configured font awesome and put trash icon on sidebar portfolio list

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const PortfolioSidebarList = props => {
     const portfolioList = props.data.map(portfolioItem => {
@@ -11,8 +12,9 @@ const PortfolioSidebarList = props => {
                 <h1 className="title">{portfolioItem.name}</h1>
                 <h2>{portfolioItem.id}</h2>
                 <a onClick={() => props.handleDeleteClick(portfolioItem)}>
-                    Delete
+                <FontAwesomeIcon icon="trash" />
                 </a>
+                
             </div>
         )
     })
